c5747892556a6a66366bebba43dc14459b18b826
Michael Qiao <honghui.qiao@outlook.com>
2019-09-19 00:44:51 +0800

    SportsStore project

---
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -0,0 +1,41 @@
+using System;
+using Xunit;
+using Moq;
+using SportsStore.Models;
+using System.Linq;
+using SportsStore.Controllers;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SportsStore.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void Can_Paginate()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products)
+                .Returns((new Product[]
+                {
+                    new Product {ProductId = 1, Name = "P1"},
+                    new Product {ProductId = 2, Name = "P2"},
+                    new Product {ProductId = 3, Name = "P3"},
+                    new Product {ProductId = 4, Name = "P4"},
+                    new Product {ProductId = 5, Name = "P5"}
+                })
+                .AsQueryable<Product>());
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            IEnumerable<Product> result = controller
+                .List(2).ViewData.Model as IEnumerable<Product>;
+
+            Product[] prodArray = result.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P4", prodArray[0].Name);
+            Assert.Equal("P5", prodArray[1].Name);
+        }
+    }
+}
